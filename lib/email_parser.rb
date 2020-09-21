b26e95a6a7b5ee70fc0e3988b2acdbb18fe9cd5d
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-21 01:44:57 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,10 +4,10 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  attr_accessor :emails
+  attr_accessor :csv_emails
   
-  def initialize(emails)
-    @emails = emails
+  def initialize(csv_emails)
+    @csv_emails = csv_emails
   end
   
   
